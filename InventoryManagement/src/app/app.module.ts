693b660a2bcc3a6c9b65729f065e2ddab73af33f
wilbert.sandoval <wilbert.sandoval@cetys.edu.mx>
2022-11-13 20:43:26 -0800

    modulo de prueba

--- InventoryManagement/src/app/app.module.ts
+++ InventoryManagement/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { prueba } from './prueba/prueba.componente';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    prueba
   ],
   imports: [
     BrowserModule,
---
+++ InventoryManagement/src/app/prueba/prueba.componente.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component(
+    {
+        selector:'prueba',
+        templateUrl:'./prueba.componente.html',
+        styleUrls:['./prueba.componente.css']
+    }
+)
+export class prueba{
+    colores=['rojo','verde','azul'];
+}
