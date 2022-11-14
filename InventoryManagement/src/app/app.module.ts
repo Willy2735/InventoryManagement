0388792b424393208df458f8b9918a6dd893afb6
wilbert.sandoval <wilbert.sandoval@cetys.edu.mx>
2022-11-14 07:04:46 -0800

    se creo header y footer

--- InventoryManagement/src/app/app.module.ts
+++ InventoryManagement/src/app/app.module.ts
@@ -3,11 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { prueba } from './prueba/prueba.componente';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { HeaderComponent } from './componentes/header/header.component';
+
 @NgModule({
   declarations: [
     AppComponent,
-    prueba
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
