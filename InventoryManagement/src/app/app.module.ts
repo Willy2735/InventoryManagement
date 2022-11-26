a98d005908b306f0c8bb6a65c60a2877a93b877e
wilbert.sandoval <wilbert.sandoval@cetys.edu.mx>
2022-11-25 18:43:22 -0800

    se completo gran parte del LOGIN

--- InventoryManagement/src/app/app.module.ts
+++ InventoryManagement/src/app/app.module.ts
@@ -3,20 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './componentes/footer/footer.component';
+
 import { HeaderComponent } from './componentes/header/header.component';
 import { ArticuloComponent } from './componentes/articulo/articulo.component';
 import { HomepageComponent } from './componentes/homepage/homepage.component';
 import { VentasComponent } from './componentes/ventas/ventas.component';
+import { LoginComponent } from './componentes/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent,
     HeaderComponent,
     ArticuloComponent,
     HomepageComponent,
-    VentasComponent
+    VentasComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
